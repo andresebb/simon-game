2edae900dfbb6390e6e75c25b0ea944dd73dc905
Andres Betancourt <aebb001@gmail.com>
2020-12-17 15:22:57 -0400

    Iluminado secuencia de colores

---
+++ app.js
@@ -0,0 +1,68 @@
+const celeste = document.getElementById("celeste");
+const violeta = document.getElementById("violeta");
+const naranja = document.getElementById("naranja");
+const verde = document.getElementById("verde");
+const btnEmpezar = document.getElementById("btnEmpezar");
+
+class Juego {
+  constructor() {
+    this.inicializar();
+    this.generarSecuencia();
+    this.siguienteNivel();
+  }
+
+  inicializar() {
+    btnEmpezar.classList.add("hide");
+    this.nivel = 10;
+    this.colores = {
+      celeste,
+      violeta,
+      naranja,
+      verde,
+    };
+  }
+
+  //Generamos un array con numeros aleatorios
+  generarSecuencia() {
+    this.secuencia = new Array(10)
+      .fill(0)
+      .map((n) => Math.floor(Math.random() * 4));
+  }
+
+  siguienteNivel() {
+    this.iluminarSecuencia();
+  }
+
+  transformarNumeroAColor(numero) {
+    switch (numero) {
+      case 0:
+        return "celeste";
+      case 1:
+        return "violeta";
+      case 2:
+        return "naranja";
+      case 3:
+        return "verde";
+    }
+  }
+
+  iluminarSecuencia() {
+    for (let i = 0; i < this.nivel; i++) {
+      const color = this.transformarNumeroAColor(this.secuencia[i]);
+      setTimeout(() => {
+        this.iluminarColor(color);
+      }, 1000 * i);
+    }
+  }
+
+  iluminarColor(color) {
+    this.colores[color].classList.add("light");
+    setTimeout(() => {
+      this.colores[color].classList.remove("light");
+    }, 350);
+  }
+}
+
+function empezarJuego() {
+  window.juego = new Juego();
+}
